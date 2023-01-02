634f976e495be00d17c2887c836ddcf61a02fa6c
Anish Bakshi <>
2023-01-02 13:59:04 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+let name = "Anish" //also let name = 'Anish'
+console.log(name)
+console.log(name.length)
+console.log(name[0])
+console.log(name[1])
+
+// template literals
+let boy1 = "Anish"
+let boy2 = "Parth"
+let sentence = `${boy2} is a friend of ${boy1}`
+console.log(sentence)
+
+// Escape Sequence Character
+let fruit = 'Bana\'na'
+console.log(fruit)
+console.log(fruit.length) // \' is also a character
+console.log(fruit.toUpperCase())
+
+let a = "Madhur"
+console.log(a.slice(2, 4))
+console.log(a.slice(2))
+console.log(a.replace("Mad", "Sad"))
+
+console.log(name.concat(" is a friend of ", a))
+
+let myfriend = "    Priya    "
+console.log(myfriend)
+console.log(myfriend.trim())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
